ff9c485e84bcb3805fbcef2cb6be462d98ea568d
Luis Lopez <luiscarloslj77777@gmail.com>
2021-06-22 16:37:24 -0600

    Finishing_feed

--- social/views.py
+++ social/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def feed(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'social/feed.html', context)
+
+
+def profile(request):
+    return render(request, 'social/profile.html')
